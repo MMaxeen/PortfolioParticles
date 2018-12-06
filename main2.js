eed1450f04fb7d0c32a8017f9d93060390d1aced
Maxime Motte <maxeen17@gmail.com>
2018-12-06 17:25:12 +0100

    menu mobile

--- main2.js
+++ main2.js
@@ -1,61 +1,61 @@
-//Bouncing Ball
-var x = 0, y = 0,
-    vx = 0, vy = 0,
-	ax = 0, ay = 0;
+// //Bouncing Ball
+// var x = 0, y = 0,
+//     vx = 0, vy = 0,
+// 	ax = 0, ay = 0;
 	
-var sphere = document.getElementById("sphere");
-var sphere2 = document.getElementById("follower");
-var window = document.getElementById("particles-js");
+// var sphere = document.getElementById("sphere");
+// var sphere2 = document.getElementById("follower");
+// var window = document.getElementById("particles-js");
 
-if (window.DeviceMotionEvent != undefined) {
-	window.ondevicemotion = function(e) {
-    ax = event.accelerationIncludingGravity.x * 20;
-		ay = event.accelerationIncludingGravity.y * 20;
-		// document.getElementById("accelerationX").innerHTML = e.accelerationIncludingGravity.x;
-		// document.getElementById("accelerationY").innerHTML = e.accelerationIncludingGravity.y;
-		// document.getElementById("accelerationZ").innerHTML = e.accelerationIncludingGravity.z;
+// if (window.DeviceMotionEvent != undefined) {
+// 	window.ondevicemotion = function(e) {
+//     ax = event.accelerationIncludingGravity.x * 20;
+// 		ay = event.accelerationIncludingGravity.y * 20;
+// 		// document.getElementById("accelerationX").innerHTML = e.accelerationIncludingGravity.x;
+// 		// document.getElementById("accelerationY").innerHTML = e.accelerationIncludingGravity.y;
+// 		// document.getElementById("accelerationZ").innerHTML = e.accelerationIncludingGravity.z;
 
-		if ( e.rotationRate ) {
-			// document.getElementById("rotationAlpha").innerHTML = e.rotationRate.alpha;
-			// document.getElementById("rotationBeta").innerHTML = e.rotationRate.beta;
-			// document.getElementById("rotationGamma").innerHTML = e.rotationRate.gamma;
-		}		
-	}
+// 		if ( e.rotationRate ) {
+// 			// document.getElementById("rotationAlpha").innerHTML = e.rotationRate.alpha;
+// 			// document.getElementById("rotationBeta").innerHTML = e.rotationRate.beta;
+// 			// document.getElementById("rotationGamma").innerHTML = e.rotationRate.gamma;
+// 		}		
+// 	}
 
-	setInterval( function() {
-		var landscapeOrientation = window.innerWidth/window.innerHeight > 1;
-		if ( landscapeOrientation) {
-			vx = vx + ay;
-			vy = vy + ax;
-		} else {
-			vy = vy - ay;
-			vx = vx + ax;
-		}
-		vx = vx * 0.98;
-		vy = vy * 0.98;
-		y = parseInt(y + vy / 50);
-		x = parseInt(x + vx / 50);
+// 	setInterval( function() {
+// 		var landscapeOrientation = window.innerWidth/window.innerHeight > 1;
+// 		if ( landscapeOrientation) {
+// 			vx = vx + ay;
+// 			vy = vy + ax;
+// 		} else {
+// 			vy = vy - ay;
+// 			vx = vx + ax;
+// 		}
+// 		vx = vx * 0.98;
+// 		vy = vy * 0.98;
+// 		y = parseInt(y + vy / 50);
+// 		x = parseInt(x + vx / 50);
 		
-		boundingBoxCheck();
+// 		boundingBoxCheck();
 		
-		sphere.style.top = y;
-    sphere.style.left = x;
+// 		sphere.style.top = y;
+//     sphere.style.left = x;
 
-    sphere2.style.top = y;
-    sphere2.style.left = x;
+//     sphere2.style.top = y;
+//     sphere2.style.left = x;
     
 		
-	}, 20);
-};
+// 	}, 20);
+// };
 
 
-function boundingBoxCheck(){
-	if (x<0) { x = 0; vx = -vx; }
-	if (y<0) { y = 0; vy = -vy; }
-	if (x>document.documentElement.clientWidth) { x = document.documentElement.clientWidth; vx = -vx; }
-	if (y>document.documentElement.clientHeight) { y = document.documentElement.clientHeight; vy = -vy; }
+// function boundingBoxCheck(){
+// 	if (x<0) { x = 0; vx = -vx; }
+// 	if (y<0) { y = 0; vy = -vy; }
+// 	if (x>document.documentElement.clientWidth) { x = document.documentElement.clientWidth; vx = -vx; }
+// 	if (y>document.documentElement.clientHeight) { y = document.documentElement.clientHeight; vy = -vy; }
 	
-}
+// }
 
 var root = document.querySelector('body');
 
